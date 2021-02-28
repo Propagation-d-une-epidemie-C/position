0c947472b789915f2466055734a9ab5e1cd91f17
smorard <78926477+smorard@users.noreply.github.com>
2021-02-28 15:23:16 +0100

    Update position.h

--- position.h
+++ position.h
@@ -1,14 +1,14 @@
 #ifndef _position_h_
 #define _position_h_ 1
-
+#include <iostream>
 #include <vector>
 
 class position {
   
   private:
   
-    const unsigned int N; // Nombre maximal de personnes
-    const unsigned int taille; // Valeur maximale à tirer
+    unsigned int N; // Nombre maximal de personnes
+    unsigned int taille; // Valeur maximale à tirer
     std::vector<int>  x; // Coordonnee x
     std::vector<int>  y; // Coordonnee y
   
@@ -20,7 +20,7 @@ class position {
     position(std::vector<int>  new_x, std::vector<int>  new_y);
   
     //si l'utilisateur veut changer N et taille
-    void initialise(unsigned int N_, unsigned int taille_) const;
+    void initialise(unsigned int N_, unsigned int taille_);
   
     //obtenir N et taille
     int get_N() const;
@@ -29,6 +29,8 @@ class position {
     // obtenir x et y
     int get_x(unsigned int i_) const;
     int get_y(unsigned int i_) const;
+    std::vector<int> getx() const;
+    std::vector<int> gety() const;
     
     // Méthode d'affichage
     void affiche() const;
@@ -36,7 +38,6 @@ class position {
     // Méthode affichant uniquement le ième tirage
     void affiche(const unsigned int i_) const;
   
-    // Comparaison de l'objet courant avec un second objet de type hasard
-    bool compare(const position & p_, unsigned int i_, double r_) const;
+
 };
 #endif
